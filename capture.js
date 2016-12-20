c1f071960ae8a5785e4c04016e729833d27017cf
Yves Schelpe <yves.schelpe@kdg.be>
2016-12-20 13:42:43 +0100

    typo

--- capture.js
+++ capture.js
@@ -21,7 +21,7 @@ function getMainSource(desktopCapturer, screen, done) {
         types: ['screen'], thumbnailSize: screen.getPrimaryDisplay().workAreaSize
     }
     desktopCapturer.getSources(options, (err, sources) => {
-        if(err) return console.log('Cannot capture screen: ', err)
+        if (err) return console.log('Cannot capture screen: ', err)
 
         const isMainSource = source => source.name === 'Entire screen' || source.name === 'Screen 1'
         done(sources.filter(isMainSource)[0])
@@ -30,7 +30,6 @@ function getMainSource(desktopCapturer, screen, done) {
 
 function writeScreenshot(png, filePath) {
     fs.writeFile(filePath, png, err => {
-        if(err) return console.log('Cannot write file to: ' + filePath)
-        
-    })    
+        if (err) return console.log('Cannot write file to: ' + filePath)
+    })
 }
